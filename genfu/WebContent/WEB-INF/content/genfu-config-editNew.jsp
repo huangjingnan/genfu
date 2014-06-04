1a76bb615951d23916d0d530fac0bbd715e0ae00
徐振富 <df.swahili@qq.com>
2014-06-04 17:38:03 +0800

    jmx 配置系统IP

--- genfu/WebContent/WEB-INF/content/genfu-config-editNew.jsp
+++ genfu/WebContent/WEB-INF/content/genfu-config-editNew.jsp
@@ -31,20 +31,20 @@ jQuery("#configListUpgrade").jqGrid({
 	url:'${ctx}/genfu-config.json?style=jqGrid',
 	datatype: "json", 
 	height: 250, 
-	colNames:['configId','configKey', 'configDescription', 'configOthers','configCreatedAt','configFlag','configParentId', 'configEffDate', 'configUpdatedAt','configSrc','configValue'],
+	colNames:['配置标识','配置键', '配置描述', '配置扩展','创建时间','配置状态','上级标识', '生效时间', '修改时间','源','值'],
 	colModel:[
    		{name:'id',index:'id', width:55,editable:true,editoptions:{readonly:true,size:10}},
-   		{name:'configKey',index:'configKey', width:80,editable:true,editoptions:{size:10}},
+   		{name:'configKey',index:'configKey', width:80,editoptions:{size:10}},
    		{name:'configDescription',index:'configDescription', width:90,hidden:true,editoptions:{size:25}},
    		{name:'configOthers',index:'configOthers', width:60,hidden:true,editoptions:{size:10}},
    		{name:'configCreatedAt',index:'configCreatedAt', width: 80,hidden:true, align: 'center', sorttype: 'date',
             formatter: 'date',editrules:{date:true,required:false}, formatoptions: {newformat: 'Y-m-d H:i:sO'}, datefmt: 'Y-m-d H:i:sO'},
-   		{name:'configFlag',index:'configFlag', width:80, editable: true,align:"left",edittype:"select",editoptions:{value:"OPEN:OPEN;CLOSED:CLOSED"}},
+   		{name:'configFlag',index:'configFlag', width:40, editable: false,align:"left",edittype:"select",editoptions:{value:"OPEN:OPEN;CLOSED:CLOSED"}},
    		{name:'configParentId',index:'configParentId',hidden:true,editable:true, align:"right",formatter:'number', width:80,editrules:{required:true,number:true,minValue:0.01}},
    		{name:'configEffDate',index:'configEffDate',hidden:true,width:100,align:"center",formatter: { srcformat:'Y-m-d', newformat: 'd/m/Y'}},
-   		{name:'configUpdatedAt',index:'configUpdatedAt',width:100,align:"center"},	
-   		{name:'configSrc',index:'configSrc', width:90,editoptions:{size:25}},
-   		{name:'configValue',index:'configValue', width:60,editoptions:{size:10}}
+   		{name:'configUpdatedAt',index:'configUpdatedAt',width:60,align:"center"},	
+   		{name:'configSrc',index:'configSrc',hidden:true, width:100,editoptions:{size:25}},
+   		{name:'configValue',index:'configValue',editable:true, width:60,editoptions:{size:100}}
    	],
    	rowNum:20,
    	rowList:[20,30,50],
@@ -54,7 +54,7 @@ jQuery("#configListUpgrade").jqGrid({
 	autowidth: true,
 	multiselect: true, caption: "configListUpgrade" }); 
 	
-jQuery("#configListUpgrade").jqGrid('navGrid','#pconfigListUpgrade',{edit:true,add:false,del:true},
+jQuery("#configListUpgrade").jqGrid('navGrid','#pconfigListUpgrade',{edit:true,add:false,del:false},
 		{url:'${ctx}/genfu-config/0?_method=put',reloadAfterSubmit:false},
 		{},
 		{url:'${ctx}/genfu-config/0?_method=DELETE&className=com.genfu.reform.model.GenfuConfig',reloadAfterSubmit:true},{multipleSearch:true, multipleGroup:false, showQuery: true});
---
+++ genfu/src/com/genfu/reform/jmx/client/Client.java
@@ -0,0 +1,128 @@
+package com.genfu.reform.jmx.client;
+
+import java.util.HashMap;
+
+import javax.management.JMX;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import com.genfu.reform.jmx.mxbeans.ConfigNetworkMXBean;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			// Environment map
+			//
+			System.out.println("\nInitialize the environment map");
+			HashMap env = new HashMap();
+
+			// Provide the credentials required by the server to successfully
+			// perform user authentication
+			//
+			String[] credentials = new String[] { "username", "password" };
+			env.put("jmx.remote.credentials", credentials);
+
+			// Create an RMI connector client and
+			// connect it to the RMI connector server
+			//
+			System.out.println("\nCreate an RMI connector client and "
+					+ "connect it to the RMI connector server");
+			JMXServiceURL url = new JMXServiceURL(
+					"service:jmx:rmi:///jndi/rmi://192.168.56.101:9999/server");
+			JMXConnector jmxc = JMXConnectorFactory.connect(url, env);
+
+			// Get an MBeanServerConnection
+			//
+			System.out.println("\nGet an MBeanServerConnection");
+			MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+
+			// Get domains from MBeanServer
+			//
+			System.out.println("\nDomains:");
+			String domains[] = mbsc.getDomains();
+			for (int i = 0; i < domains.length; i++) {
+				System.out.println("\tDomain[" + i + "] = " + domains[i]);
+			}
+
+			// Create ConfigNetwork MBean
+			//
+			ObjectName mbeanName = new ObjectName(
+					"com.genfu.agent.mxbeans:type=ConfigNetwork");
+			System.out.println("\nCreate ConfigNetwork MBean...");
+			mbsc.createMBean("com.genfu.agent.mxbeans.ConfigNetwork",
+					mbeanName, null, null);
+
+			// Get MBean count
+			//
+			System.out.println("\nMBean count = " + mbsc.getMBeanCount());
+
+			// Get State attribute
+			//
+			// System.out.println("\nState = " +
+			// mbsc.getAttribute(mbeanName, "State"));
+
+			// Set State attribute
+			//
+			// mbsc.setAttribute(mbeanName,
+			// new Attribute("State", "changed state"));
+
+			// Get State attribute
+			//
+			// Another way of interacting with a given MBean is through a
+			// dedicated proxy instead of going directly through the MBean
+			// server connection
+			//
+			ConfigNetworkMXBean proxy = JMX.newMBeanProxy(mbsc, mbeanName,
+					ConfigNetworkMXBean.class);
+			// System.out.println("\nState = " + proxy.getState());
+
+			// Add notification listener on ConfigNetwork MBean
+			//
+			// ClientListener listener = new ClientListener();
+			// System.out.println("\nAdd notification listener...");
+			// mbsc.addNotificationListener(mbeanName, listener, null, null);
+
+			// Invoke "reset" in ConfigNetwork MBean
+			//
+			// Calling "reset" makes the ConfigNetwork MBean emit a
+			// notification that will be received by the registered
+			// ClientListener.
+			//
+			Object[] params = new Object[] { "192.168.56.102", "255.255.255.0",
+					"192.168.56.1" };
+			System.out
+					.println("\nInvoke cfgDebian() in ConfigNetwork MBean...");
+			mbsc.invoke(mbeanName, "cfgDebian", params, new String[] {
+					String.class.getName(), String.class.getName(),
+					String.class.getName() });
+
+			// Sleep for 2 seconds in order to have time to receive the
+			// notification before removing the notification listener.
+			//
+			System.out.println("\nWaiting for notification...");
+			Thread.sleep(2000);
+
+			// Remove notification listener on ConfigNetwork MBean
+			//
+			// System.out.println("\nRemove notification listener...");
+			// mbsc.removeNotificationListener(mbeanName, listener);
+
+			// Unregister ConfigNetwork MBean
+			//
+			System.out.println("\nUnregister ConfigNetwork MBean...");
+			mbsc.unregisterMBean(mbeanName);
+
+			// Close MBeanServer connection
+			//
+			System.out.println("\nClose the connection to the server");
+			jmxc.close();
+			System.out.println("\nBye! Bye!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
